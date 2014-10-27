bad8718b2bf99e402d4421a94b0e3fcaa6fe9249
gehpark <gehpark@gmail.com>
2014-10-27 11:33:51 -0700

    Sort of established host

--- host.py
+++ host.py
@@ -1,26 +1,45 @@
 import simpy
 
-class Host:
-    """Simulator object representing a host"""
+class Host(object):
+    """Simulator object representing a host
+    A host can only send 1 packet at a timeout
 
-    def __init__(self, env, addr):
+    Flows have to request for the host. When they have access
+    to one, they can start the sending process and wait for it 
+    to finish and to receive the acknowledgement packet
+    """
+
+    def __init__(self, env, addr, sendtime, receivetime):
         self.env = env
-
         # host name. DNS translates this into an IP address
         self.name = name
         # host address
         self.addr = addr
         # Queue to hold packets to transmit
         self.packets = Queue()
+        # Create a shared resource to limit flows' access
+        self.host = simpy.Resource(env, 1)
+        # This is the time it takes for the host to send a packet
+        self.sendtime = sendtime
+        # This is the time it takes for a host to receive a packet
+        self.receivetime = receivetime
 
-    def transmit(self, env):
+    def transmit(self, packet):
         """Transmit packets to their destination."""
-        pass
+        yield self.env.timeout(self.sendtime)
 
-    def receive(self, env):
-        """Receive & forwards an inbound (acknowledgement) packet."""
-        pass
-
-    def acknowledge(self, env):
+    def transmitack(self, env, packet, senderaddr):
         """Transmit acknowledgement packet of size 64 bytes"""
-        pass
+        yield self.env.timeout(self.sendtime)
+
+    def receive(self, env, packet, senderaddr):
+        """Receive inbound packet."""
+        ack = Packet()
+        yield self.env.timeout(self.receivetime)
+        transmitack(self, env, ack, senderaddr)
+
+    def receiveack(self, env):
+        """ Receive an inbound acknowledgement packet"""
+        while True:
+            if (ack is received):
+                break
