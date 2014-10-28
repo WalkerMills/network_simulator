92570e5003d8946bc2817be481c7f953fdd5ac42
Walker Mills <walker.mills7@gmail.com>
2014-10-28 16:32:56 -0700

    Added documentation to router module
    I wrote some docstrings for the Router & ReceivePacket classes, as well
    as some inline comments.

--- router.py
+++ router.py
@@ -2,6 +2,13 @@ import simpy
 
 
 class ReceivePacket(simpy.events.Event):
+    """Simulator event representing packet reception.
+
+    This event takes a router as a parameter, and represents the addition
+    of a packet to that router.  It also triggers a packet transmission
+    for each packet received.  This event may also be used as a context
+    manager.
+    """
 
     def __init__(self, resource, packet):
         # Initialize event
@@ -28,13 +35,21 @@ class ReceivePacket(simpy.events.Event):
 
 
 class Router(object):
+    """Simulator object representing a router.
 
+    Router is implemented as a unidirectional resource.  It receives
+    packets, but does not offer packet transmission requests.
+    """
+    # Queue of packet events to process
     PacketQueue = list
 
     def __init__(self, env):
         self._env = env
         self.packets = self.PacketQueue()
+        # List of outbound links of the form (link cost, link)
         self.links = list()
+        # Dictionary mapping outbound links to destination ranges
+        self.table = dict()
 
         # Bind event constructors as methods
         simpy.core.BoundClass.bind_early(self)
@@ -43,18 +58,15 @@ class Router(object):
 
     def _send(self, packet):
         """Send an outbound packet."""
-
         # TODO: route packet, send it through a link
         pass
 
     def _trigger_send(self):
         """Trigger outbound packet transmission."""
-
         event = self.packets.pop(0)
         self._send(event.packet)
         event.succeed()
 
     def register_link(self, transport):
         """Register an outbound link (transport handler)."""
-
-        self.links.append((transport, transport.cost()))
+        self.links.append((transport.cost(), transport))
