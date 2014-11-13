3bc85da9f90868a6fd35b77dbde4e7ce88c50d19
Walker Mills <walker.mills7@gmail.com>
2014-11-13 05:40:39 -0800

    Initial implementation of network creation
    Network.build_network takes an adjacency list (as returned by gui.draw)
    and a list of flows, and contructs a network of SimPy processes.
    Network.simulate initializes the flows as processes, and runs the
    simulation.

--- network.py
+++ network.py
@@ -1,50 +1,110 @@
+import logging
 import simpy
 
 import gui
 import process
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 class Network(object):
 
     def __init__(self, network=None):
-        if network is None:
-            self._input = gui.draw()
-        else:
-            self._input = network
-
         # Simulation environment
-        self.env = simpy.Environment()
-        # List of hosts in this network
-        self._hosts = list()
-        # List of routers in this network
-        self._routers = list()
+        self._env = simpy.Environment()
+        # Table of hosts in this network
+        self._hosts = dict()
+        # Table of routers in this network
+        self._routers = dict()
         # List of links in this network
         self._links = list()
         # List of flows in this network
         self._flows = list()
 
-    def build_network(self):
-        # TODO: build a network from the input adjacency list & flows 
+        # If no parameters were specified
+        if network is None:
+            # Get edges & flows from GUI input
+            edges, flows = gui.draw()
+        else:
+            edges, flows = network
+        # Populate the network
+        self._build_network(edges, flows)
+
+    def _build_network(self, edges, flows):
+        # Set of host addresses
         hosts = set()
+        # Set of router addresses
         routers = set()
-        links, flows = self._input
-        links = [(','.split(l[0]), l[1:]) for l in links]
-        flows = [(','.split(f[0]), f[1:]) for f in flows]
+        # Do some preprocessing on the given adjacency list & flows
+        edges = [(l[0].split(','), l[1:]) for l in edges]
+        flows = [(f[0].split(','), f[1:]) for f in flows]
 
-        for endpoints, _ in links:
+        # Parse the inputted addresses from the adjacency list
+        for endpoints, _ in edges:
+            # For each of the two endpoints
             for node in endpoints:
+                # Add the tag's address to the appropriate set
                 if node.startswith('h'):
-                    hosts.add(node)
+                    hosts.add(int(node[1]))
                 else:
-                    routers.add(node)
+                    routers.add(int(node[1]))
 
-        for host in sorted(hosts):
-            addr = int(host[1])
-            self._hosts.append(process.Host(self._env, addr))
-        for router in sorted(routers):
-            addr = int(router[1])
-            self._routers.append(process.Router(self._env, addr))
+        # For each host address
+        for addr in hosts:
+            logger.info('creating host {}'.format(addr))
+            # Make a new host with the given address
+            self._hosts[addr] = process.Host(self._env, addr)
+        # For each router address
+        for addr in routers:
+            logger.info('creating router {}'.format(addr))
+            # Make a new router with the given address
+            self._routers[addr] = process.Router(self._env, addr)
+
+        # For each entry in the adjacency list
+        for tags, parameters in edges:
+            # Make a new link
+            link = process.Link(self._env, *parameters)
+            # Initialize a list of endpoints
+            endpoints = list()
+
+            log_args = list()
+
+            # Add an endpoint for each tag 
+            for tag in tags:
+                # Retrieve the address from this tag
+                addr = int(tag[1])
+                # If it's a host tag
+                if tag.startswith("h"):
+                    # Append the host with the tagged address
+                    endpoints.append(self._hosts[addr])
+                    log_args.append("host")
+                # Otherwise, it's a router
+                else:
+                    # Append the router with the tagged address
+                    endpoints.append(self._routers[int(tag[1])])
+                    log_args.append("router")
+                log_args.append(addr)
+
+            logger.info('connecting {} {} and {} {}'.format(*log_args))
+            # Connect the new link to its two endpoints
+            link.connect(*endpoints)
+            # Persist the new link
+            self._links.append(link)
+
+        # For each inputted flow
+        for (src_tag, dest_tag), parameters in flows:
+            # Get the source host
+            src = self._hosts[int(src_tag[1])]
+            # Get the destination address
+            dest = int(dest_tag[1])
+            logger.info('creating flow between hosts {} and {}'.format(
+                src.addr, dest))
+            # Create & persist the new flow
+            self._flows.append(process.Flow(self._env, src, dest, *parameters))
 
     def simulate(self, until=None):
-        # TODO: run the network simulation
-        pass
+        # Initialize packet generating processes for each flow
+        processes = [self._env.process(f.generate()) for f in self._flows]
+        # Run the simulation
+        self._env.run()
