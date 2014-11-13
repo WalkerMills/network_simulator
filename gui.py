e5222e1716aef29906e82ccca9c2ad1a55b3a715
Walker Mills <walker.mills7@gmail.com>
2014-11-12 19:46:28 -0800

    Implements flow creation in the GUI
    The GUI doesn't look too pretty, but it is fully working now.  Users can
    create hosts & routers, connect them with links, and create flows from
    one host to another.

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import abc
 import logging
+import operator
 import tkinter
 
 logger = logging.getLogger(__name__)
@@ -117,39 +118,55 @@ class Dialog(tkinter.Toplevel, metaclass=abc.ABCMeta):
         pass
 
 
+class InputDialog(Dialog):
+    """Dialog for entering field data."""
+
+    fields = list()
+
+    def _get_entry(self, i):
+        """Get the (integer) value of the ith entry field."""
+        return int(getattr(self, 'e{}'.format(i)).get())
+
+    def body(self, master):
+        """Create a labeled entry box for each field."""
+        for i, att in enumerate(self.fields):
+            # Make a label for this attribute
+            tkinter.Label(master, text=att + ':').grid(row=i)
+            # Make an entry field for this attribute
+            entry = 'e{}'.format(i)
+            setattr(self, entry, tkinter.Entry(master))
+            # Position this entry field
+            getattr(self, entry).grid(row=i, column=1)
+
+    def apply(self):
+        """Store the values entered in the entry fields."""
+        # Store the entered parameters in this dialog's result
+        self.result = [self._get_entry(i) for i in range(len(self.fields))]
+
+
+
 class NetworkInput(tkinter.Frame):
     """This class implements a GUI to draw a network configuration."""
 
-
-    class LinkDialog(Dialog):
+    class LinkDialog(InputDialog):
         """Dialog for specifying link parameters."""
 
-        attributes = ['Capacity', 'Delay', 'Buffer size']
-
-        def _get_entry(self, i):
-            """Get the (integer) value of the ith entry field."""
-            return int(getattr(self, 'e{}'.format(i)).get())
-
-        def body(self, master):
-            for i, att in enumerate(self.attributes):
-                # Make a label for this attribute
-                tkinter.Label(master, text=att + ':').grid(row=i)
-                # Make an entry field for this attribute
-                entry = 'e{}'.format(i)
-                setattr(self, entry, tkinter.Entry(master))
-                # Position this entry field
-                getattr(self, entry).grid(row=i, column=1)
+        fields = ['Capacity', 'Delay', 'Buffer size']
 
         def validate(self):
-            # Check that all values are >= 0
+            """Check that all values entered are >= 0"""
             return all(map(lambda i: self._get_entry(i) >= 0,
-                           range(len(self.attributes))))
+                           range(len(self.fields))))
 
-        def apply(self):
-            # Store the entered parameters in this dialog's result
-            self.result = [self._get_entry(i) for i in 
-                           range(len(self.attributes))]
+    class FlowDialog(InputDialog):
+        """Dialog for specifying flow parameters"""
 
+        fields = ['Window', 'Timeout', 'Data']
+
+        def validate(self):
+            """Check that all values entered are >= 0"""
+            return all(map(lambda i: self._get_entry(i) >= 0,
+                           range(len(self.fields))))
 
     def __init__(self, master, width_=600, height_=400):
         # Initialize GUI
@@ -158,16 +175,18 @@ class NetworkInput(tkinter.Frame):
         self.canvas.pack(fill='both', expand='1')
 
         # Side length of hosts & routers in pixels
-        self.dim = 10
+        self._dim = 10
         # Starting coordinates for a link
-        self.start = None
-
+        self._start = None
+        # Initial host for a flow
+        self._src = None
         # Current number of hosts
-        self.hosts = 0
+        self._hosts = 0
         # Current number of routers
-        self.routers = 0
+        self._routers = 0
         # Adjacency list (implicitly) representing links
         self.links = list()
+        # List of flows in the network
         self.flows = list()
 
         # Bind 'h' key to host creation
@@ -176,64 +195,115 @@ class NetworkInput(tkinter.Frame):
         self.master.bind('r', self.draw_router)
         # Bind left mouse button to link creation
         self.canvas.bind('<Button-1>', self.draw_link)
+        # Bind right mouse button to flow creation
+        self.canvas.bind('<Button-3>', self.make_flow)
 
     def _draw_component(self, x, y, color, tag):
         """Draw a rectangular component."""
-        self.canvas.create_rectangle(x, y, x + self.dim, y + self.dim,
+        self.canvas.create_rectangle(x, y, x + self._dim, y + self._dim,
             fill=color, tags=tag)
+
+    def _find_item(self, x, y, tag, invert=False):
+        """Find an item (prefix) matching a given tag.
+
+        This method finds all items near the coordinates given, and
+        returns the first tag of the closest item whose first tag prefix
+        matches the tag given.  If the invert parameter is True, then
+        the matching is inverted, and the first item whose first tag
+        doesn't prefix match tag is returned.  If no such item is found,
+        this method returns None.
+        """
+        # If invert is true, check for an item that doesn't match <tag>
+        truth = operator.not_ if invert else operator.truth
+        # Find nearby items
+        nearby = self.canvas.find_overlapping(
+            x - self._dim / 2, y - self._dim / 2,
+            x + self._dim / 2, y + self._dim / 2)
+        for item in nearby:
+            # Retrieve the item's tags
+            tags = self.canvas.gettags(item)
+            # If the item matches correctly to the given tag
+            if truth(tags[0].startswith(tag)):
+                # This item is an endpoint
+                return tags[0]
+        # Return None if no valid endpoint was found
+        return None
 
     def draw_host(self, event):
         """Draw a host."""
         self._draw_component(event.x, event.y, '#66FF33',
-                             'h{}'.format(self.hosts))
+                             'h{}'.format(self._hosts))
         # Increment host count
-        self.hosts += 1
+        self._hosts += 1
 
     def draw_router(self, event):
         """Draw a router."""
         self._draw_component(event.x, event.y, '#3366FF',
-                             'r{}'.format(self.routers))
+                             'r{}'.format(self._routers))
         # Increment router count
-        self.routers += 1
+        self._routers += 1
 
     def draw_link(self, event):
         """Draw a link."""
-        # If there is no link in progress, a click represents a start point
-        if self.start is None:
-            self.start = event.x, event.y
+        # If there is no link in progress
+        if self._start is None:
+            # The click event is the start point of a link
+            self._start = event.x, event.y
             return
 
         link = list()
-        for point in self.start, (event.x, event.y):
-            # Find all nearby items
-            nearby = self.canvas.find_overlapping(
-                point[0] - self.dim / 2, point[1] - self.dim / 2,
-                point[0] + self.dim / 2, point[1] + self.dim / 2)
-
-            for item in nearby:
-                # Retrieve the item's tags
-                t = self.canvas.gettags(item)
-                # If the item is not a link
-                if not t[0].startswith('l'):
-                    # This item is one of our link endpoints
-                    link.append(t[0])
-                    break
+        for point in self._start, (event.x, event.y):
+            endpoint = self._find_item(point[0], point[1], 'l', invert=True)
+            if endpoint is not None:
+                link.append(endpoint)
 
         # If we have two valid endpoints
         if len(link) == 2:
-            logger.info('creating new link between {} and {}'.format(*link))
-            link = ','.join(link)
             # Get link parameters
             dialog = NetworkInput.LinkDialog(self)
-            # Draw a link in the GUI
-            self.canvas.create_line(self.start[0], self.start[1], event.x,
-                                    event.y, fill='black', tags='l,' + link)
-            # Update the list of links
-            self.links.append([link] + dialog.result)
+            # If we got valid link parameters
+            if dialog.result is not None:
+                logger.info('creating new link from {} to {}'.format(*link))
+                link = ','.join(link)
+                # Draw a link in the GUI
+                self.canvas.create_line(self._start[0], self._start[1], 
+                                        event.x, event.y, fill='black', 
+                                        tags='l,' + link)
+                # Update the list of links
+                self.links.append([link] + dialog.result)
+            else:
+                logger.info('link creation failed; invalid link parameters')
         else:
             logger.info('link creation failed; endpoints not valid')
         # Reset start coordinates
-        self.start = None
+        self._start = None
+
+    def make_flow(self, event):
+        """Make a flow from one host to another."""
+        # Find the closest host
+        endpoint = self._find_item(event.x, event.y, 'h')
+
+        # If we found a host
+        if endpoint is not None:
+            # If there is no flow in progress
+            if self._src is None:
+                # The endpoint we found is the source of a flow
+                self._src = endpoint
+                return
+
+            # Get flow parameters
+            dialog = NetworkInput.FlowDialog(self)
+            # If we got valid flow parameters
+            if dialog.result is not None:
+                logger.info('creating new flow from {} to {}'.format(self._src,
+                                                                     endpoint))
+                # Update the list of flows
+                self.flows.append([self._src + ',' + endpoint] + dialog.result)
+        else:
+            logger.info('invalid flow endpoint')
+        # Reset flow source
+        self._src = None
+
 
 def draw():
     """Draw the network configuration GUI."""
@@ -243,6 +313,7 @@ def draw():
     canvas.pack(fill='both', expand='1')
     # Run GUI
     tkinter.mainloop()
+    return canvas.links, canvas.flows
 
 if __name__ == "__main__":
     draw()
