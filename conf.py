3dd24b2321dd0f89c249246c738cdfb05b95632a
Walker Mills <walker.mills7@gmail.com>
2014-11-17 20:23:27 -0800

    Updates mock modules in conf.py

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['simpy', 'tkinter', '_tkinter']
+MOCK_MODULES = ['simpy', 'simpy.util', 'tkinter', '_tkinter']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # The suffix of source filenames.
