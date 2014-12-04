bf0744d15fcd1a726bac2f24765703b75a126189
Walker Mills <walker.mills7@gmail.com>
2014-12-04 10:59:50 -0800

    Updates requirements.txt, mock modules

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['_tkinter']
+MOCK_MODULES = ['_tkinter', 'numpy', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # The suffix of source filenames.
