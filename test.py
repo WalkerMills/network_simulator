351bcc53bb620a2556c48cbf56fa54fe703b3fb7
Walker Mills <walker.mills7@gmail.com>
2014-12-06 13:23:51 -0800

    Moves Network class to test module
    Given that the test module has become the home of all other simulation
    testing functionality, it seems appropriate to move the Network class
    there, and remove the Network module, especially since Network is
    heavily dependent on things defined in test, and test cannot function
    without the Network class.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 """
 .. module:: test
     :platform: Unix
-    :synopsis: Test cases & monitoring for the network simulator.
+    :synopsis: Testing utilites for the network simulator
 """
 
 import enum
@@ -12,8 +12,9 @@ import simpy
 
 from matplotlib import pyplot as plt
 
-import network
+import gui
 import process
+import resources
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -38,82 +39,24 @@ TWO = Case.two
 """Convenience alias for test case 2 (type)."""
 
 
-class TestCase:
-    """Factory class for producing test case adjacency lists.
-
-    See ``adjacent`` for details.
-    """
-
-    adjacencies = {
-        Case.zero: ([(('h0', 'h1'), (10000000, 512000, 10000000))], 
-                    [(('h0', 'h1'), (160000000, 1000000000))]),
-        Case.one: ([(('h0', 'r0'), (12500000, 512000, 10000000)),
-                    (('r0', 'r1'), (10000000, 512000, 10000000)),
-                    (('r0', 'r3'), (10000000, 512000, 10000000)),
-                    (('r1', 'r2'), (10000000, 512000, 10000000)),
-                    (('r3', 'r2'), (10000000, 512000, 10000000)),
-                    (('r2', 'h1'), (12500000, 512000, 10000000))],
-                   [(('h0', 'h1'), (160000000, 500000000))]),
-        Case.two: ([(('r0', 'r1'), (10000000, 1024000, 10000000)),
-                    (('r1', 'r2'), (10000000, 1024000, 10000000)),
-                    (('r2', 'r3'), (10000000, 1024000, 10000000)),
-                    (('h0', 'r0'), (12500000, 1024000, 10000000)),
-                    (('h1', 'r0'), (12500000, 1024000, 10000000)),
-                    (('h2', 'r2'), (12500000, 1024000, 10000000)),
-                    (('h3', 'r3'), (12500000, 1024000, 10000000)),
-                    (('h4', 'r1'), (12500000, 1024000, 10000000)),
-                    (('h5', 'r3'), (12500000, 1024000, 10000000))],
-                   [(('h0', 'h3'), (280000000, 500000000)),
-                    (('h1', 'h4'), (120000000, 10000000000)),
-                    (('h2', 'h5'), (240000000, 20000000000))])
-    }
-    """Edge & flow adjacency lists for each test case."""
-
-    tcp_parameters = {
-        Case.zero: {'FAST': [[1, 30000000, 45]], 'Reno': [[1, 30000000]]}, 
-        Case.one: {'FAST': [[1, 120000000, 20]], 'Reno': [[1, 120000000]]},
-        Case.two: {'FAST': [[1, 150000000, 6], 
-                            [1, 90000000, 6],
-                            [1, 90000000, 6]],
-                   'Reno': [[1, 1500000000], 
-                            [1, 90000000],
-                            [1, 90000000]]}
-    }
-    """TCP parameters for each test case."""
-
-    @classmethod
-    def adjacent(cls, case, tcp="FAST"):
-        """Generate an adjanceny list for a given test case.
-
-        For the given test case & TCP specifier, an adjacency list of
-        the type taken by :class:`network.Network` is constructed from
-        the adjacency lists in :attr:`adjacencies` and the TCP parameters
-        in :attr:`tcp_parameters`.
-
-        :param case: test case to build adjacency lists for
-        :type case: :class:`Case`
-        :param str tcp: TCP specifier (see :data:`process.Flow.allowed_tcp`)
-        :return: Adjacency lists defining edges & flows
-        :rtype: ([((str, str), (int, int, int))], 
-                 [((str, str), ((int, int), (str, list)))])
-        """
-        # Check for valid parameters
-        if not isinstance(case, Case):
-            raise ValueError("invalid test case \'{}\'".format(case))
-        if tcp not in process.Flow.allowed_tcp.keys():
-            raise ValueError("invalid TCP specifier \'{}\'".format(tcp))
-
-        # Initialize list of flows
-        flows = list()
-        for (tags, param), tcp_param in zip(cls.adjacencies[case][1], 
-                                            cls.tcp_parameters[case][tcp]):
-            # Add a flow with the proper TCP parameters
-            flows.append((tags, (param, (tcp, tcp_param))))
-        # Return adjacency lists of edges & initialized flows
-        return cls.adjacencies[case][0], flows
-
-
 class Graph:
+    """Graphing class for generating simulation results.
+
+    This class creates uses the :class:`Network` class to create and run
+    a simulation with the specified parameters, then groups the values
+    monitored during that simulation by category (window title), assuming
+    that all monitored identifiers have the form \"window title,tags\",
+    where tags may itself be a comma-delimited string of identifiers,
+    such as an index, address, etc.  The instantiated object then provides
+    various methods to graph/save the data sets using matplotlib.
+
+    :param adjacent: an adjacency list of the format taken by :class:`Network`
+    :type adjacent: ([((str, str), (int, int, int))], 
+        [((str, str), ((int, int), (str, list)))]), or :class:`Case`
+    :param str tcp: TCP specifier. Used iff adjacent is a :class:`Case`
+    :param until: time or event to run the simulation until
+    :type until: int or ``simpy.events.Event``
+    """
 
     title_args = {"Flow received": ["flow", "Mbps", 1000],
                   "Flow transmitted": ["flow", "Mbps", 1000],
@@ -131,7 +74,7 @@ class Graph:
         # Data sets grouped by window title (data category)
         self._data = dict()
         # Create a network simulation with the given parameters
-        n = network.Network(adjacent, tcp)
+        n = Network(adjacent, tcp)
         # Run the simulation, and get the monitored values
         monitored = n.simulate(until)
         # For each monitored value
@@ -244,3 +187,230 @@ class Graph:
                 args = [str(), str()]
             # Graph the data sets
             self.graph(title, datasets, *args, save=save)
+
+
+class Network:
+    """This class encapsulates a network simulation.
+
+    If network is None, then a GUI for drawing a network appears.  The
+    adjacency list should be formatted as [((src, dest), (capacity, buffer
+    size, delay))], where src & dest are formatted as a string with a
+    leading \"h\" or \"r\", specifying a host or router, followed by an
+    integer id.  Flows should be given as [((src, dest), ((data, delay),
+    (tcp, tcp_param)))], where src & dest are formatted as previously, but
+    only host tags are allowed.  Link arguments are all in bits/bps, as
+    is the data parameter for flows.  Delay is given in simulation time.
+    The tcp parameter is a string which determines which TCP algorithm to
+    use; see :data:`process.Flow.allowed_tcp` for accepted TCP specifiers.
+    The tcp_param parameter is a list containing the arguments for
+    initializing the TCP object, and will change depending on the TCP
+    algorithm specified.  See :mod:`process` for details of the currently
+    implemented TCP algorithms.  Alternatively, adjacent may be a test
+    case, as defined by the enumerated type :class:`Case`.  If so the
+    ``tcp`` parameter may be a (valid) TCP specifier, used for all flows
+    in the given test case.
+
+    :param adjacent: adjacency lists of links & flows defining a network
+    :type adjacent: ([((str, str), (int, int, int))], 
+        [((str, str), ((int, int), (str, list)))]), or :class:`Case`
+    :param str tcp: TCP specifier. Used iff adjacent is a :class:`Case`
+    """
+
+    def __init__(self, adjacent=None, tcp='FAST'):
+        # Simulation environment
+        self.env = resources.MonitoredEnvironment()
+        # Table of hosts in this network
+        self._hosts = dict()
+        # Table of routers in this network
+        self._routers = dict()
+        # List of links in this network
+        self._links = list()
+        # List of flows in this network
+        self._flows = list()
+
+        # If no parameters were specified
+        if adjacent is None:
+            # Get edges & flows from GUI input
+            adjacent = gui.draw()
+        # Alternatively, if a test case was specified
+        elif isinstance(adjacent, Case):
+            # Build the corresponding adjacency list
+            adjacent = TestCase.adjacent(adjacent, tcp)
+        # Populate the network
+        self._build_network(*adjacent)
+
+    def _build_network(self, edges, flows):
+        """Build a network of SimPy processes."""
+        # Set of host addresses
+        hosts = set()
+        # Set of router addresses
+        routers = set()
+
+        # Parse the inputted addresses from the adjacency list
+        for endpoints, _ in edges:
+            # For each of the two endpoints
+            for node in endpoints:
+                # Add the tag's address to the appropriate set
+                if node.startswith('h'):
+                    hosts.add(int(node[1:]))
+                else:
+                    routers.add(int(node[1:]))
+
+        # For each host address
+        for addr in hosts:
+            logger.debug('creating host {}'.format(addr))
+            # Make a new host with the given address
+            self._hosts[addr] = process.Host(self.env, addr)
+        # For each router address
+        for addr in routers:
+            logger.debug('creating router {}'.format(addr))
+            # Make a new router with the given address
+            self._routers[addr] = process.Router(self.env, addr)
+
+        # For each entry in the adjacency list
+        for tags, parameters in edges:
+            # Add link addr to parameters used to create a link
+            parameters = list(parameters) + [len(self._links)]
+            # Make a new link
+            link = process.Link(self.env, *parameters)
+            # Initialize a list of endpoints
+            endpoints = list()
+            # Logger parameters
+            log_args = list()
+            # Add an endpoint for each tag 
+            for tag in tags:
+                # Retrieve the address from this tag
+                addr = int(tag[1:])
+                # If it's a host tag
+                if tag.startswith("h"):
+                    # Append the host with the tagged address
+                    endpoints.append(self._hosts[addr])
+                    log_args.append("host")
+                # Otherwise, it's a router
+                else:
+                    # Append the router with the tagged address
+                    endpoints.append(self._routers[addr])
+                    log_args.append("router")
+                log_args.append(addr)
+
+            logger.debug('connecting {} {} and {} {}'.format(*log_args))
+            # Connect the new link to its two endpoints
+            link.connect(*endpoints)
+            # Persist the new link
+            self._links.append(link)
+
+        # For each inputted flow
+        for (src_tag, dest_tag), ((data, delay), (tcp, tcp_param)) in flows:
+            # Get the source host
+            src = self._hosts[int(src_tag[1])]
+            # Get the destination address
+            dest = int(dest_tag[1])
+            logger.debug('creating flow between hosts {} and {}'.format(
+                src.addr, dest))
+            # Create & persist the new flow
+            self._flows.append(process.Flow(self.env, src, dest, data, delay,
+                                            tcp, tcp_param))
+
+    def simulate(self, until=None):
+        """Run the initialized simulation.
+
+        :param until: time or event to run the simulation until
+        :type until: int or ``simpy.events.Event``
+        :return: all monitored values
+        :rtype: dict
+        """
+        # Begin routing table initialization
+        router_proc = \
+            [self.env.process(r.begin()) for r in self._routers.values()]
+        # Initialize packet generating processes for each flow
+        flow_proc = [self.env.process(f.generate()) for f in self._flows]
+        # If we didn't get a termination condition
+        if until is None:
+            # Terminate once all flows are finished transmitting data
+            until = simpy.events.AllOf(self.env, 
+                                       [f.finished for f in self._flows])
+        # Run the simulation
+        self.env.run(until=until)
+        logger.info("all flows have terminated at time {}".format(
+            self.env.now))
+        # Retrieve monitored values
+        values = self.env.monitored()
+        # Reset the environment
+        self.env = resources.MonitoredEnvironment()
+        # Return the monitored values
+        return values
+
+
+class TestCase:
+    """Factory class for producing test case adjacency lists.
+
+    See :meth:`adjacent` for details.
+    """
+
+    adjacencies = {
+        Case.zero: ([(('h0', 'h1'), (10000000, 512000, 10000000))], 
+                    [(('h0', 'h1'), (160000000, 1000000000))]),
+        Case.one: ([(('h0', 'r0'), (12500000, 512000, 10000000)),
+                    (('r0', 'r1'), (10000000, 512000, 10000000)),
+                    (('r0', 'r3'), (10000000, 512000, 10000000)),
+                    (('r1', 'r2'), (10000000, 512000, 10000000)),
+                    (('r3', 'r2'), (10000000, 512000, 10000000)),
+                    (('r2', 'h1'), (12500000, 512000, 10000000))],
+                   [(('h0', 'h1'), (160000000, 500000000))]),
+        Case.two: ([(('r0', 'r1'), (10000000, 1024000, 10000000)),
+                    (('r1', 'r2'), (10000000, 1024000, 10000000)),
+                    (('r2', 'r3'), (10000000, 1024000, 10000000)),
+                    (('h0', 'r0'), (12500000, 1024000, 10000000)),
+                    (('h1', 'r0'), (12500000, 1024000, 10000000)),
+                    (('h2', 'r2'), (12500000, 1024000, 10000000)),
+                    (('h3', 'r3'), (12500000, 1024000, 10000000)),
+                    (('h4', 'r1'), (12500000, 1024000, 10000000)),
+                    (('h5', 'r3'), (12500000, 1024000, 10000000))],
+                   [(('h0', 'h3'), (280000000, 500000000)),
+                    (('h1', 'h4'), (120000000, 10000000000)),
+                    (('h2', 'h5'), (240000000, 20000000000))])
+    }
+    """Edge & flow adjacency lists for each test case."""
+
+    tcp_parameters = {
+        Case.zero: {'FAST': [[1, 30000000, 45]], 'Reno': [[1, 30000000]]}, 
+        Case.one: {'FAST': [[1, 120000000, 20]], 'Reno': [[1, 120000000]]},
+        Case.two: {'FAST': [[1, 150000000, 6], 
+                            [1, 90000000, 6],
+                            [1, 90000000, 6]],
+                   'Reno': [[1, 1500000000], 
+                            [1, 90000000],
+                            [1, 90000000]]}
+    }
+    """TCP parameters for each test case."""
+
+    @classmethod
+    def adjacent(cls, case, tcp="FAST"):
+        """Generate an adjanceny list for a given test case.
+
+        For the given test case & TCP specifier, an adjacency list of
+        the type taken by :class:`network.Network` is constructed from
+        the adjacency lists in :attr:`adjacencies` and the TCP parameters
+        in :attr:`tcp_parameters`.
+
+        :param case: test case to build adjacency lists for
+        :type case: :class:`Case`
+        :param str tcp: TCP specifier (see :data:`process.Flow.allowed_tcp`)
+        :return: Adjacency lists defining edges & flows
+        :rtype: ([((str, str), (int, int, int))], 
+                 [((str, str), ((int, int), (str, list)))])
+        """
+        # Check for valid parameters
+        if not isinstance(case, Case):
+            raise ValueError("invalid test case \'{}\'".format(case))
+        if tcp not in process.Flow.allowed_tcp.keys():
+            raise ValueError("invalid TCP specifier \'{}\'".format(tcp))
+
+        # Initialize list of flows
+        flows = list()
+        for (tags, param), tcp_param in zip(cls.adjacencies[case][1], 
+                                            cls.tcp_parameters[case][tcp]):
+            # Add a flow with the proper TCP parameters
+            flows.append((tags, (param, (tcp, tcp_param))))
+        # Return adjacency lists of edges & initialized flows
+        return cls.adjacencies[case][0], flows
